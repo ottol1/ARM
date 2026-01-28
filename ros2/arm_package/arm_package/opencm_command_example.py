68f92ff79a8da6707de57d3c5ab273d0bcc2b2ce
Bruce Noble <145707221+MoonGoose515@users.noreply.github.com>
2026-01-28 09:12:58 -0700

    Add files via upload

--- ros2/arm_package/arm_package/opencm_command_example.py
+++ ros2/arm_package/arm_package/opencm_command_example.py
@@ -8,15 +8,15 @@ class TrajectoryToSerialNode(Node):
     def __init__(self):
         super().__init__('trajectory_to_serial_node')
         
-        # 1. Persistent Serial Connection
+        # persistent serial connection
         try:
-            self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-            self.get_logger().info("Connected to /dev/ttyACM0")
+            self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) # try to make more adaptive?
+            self.get_logger().info("Connected to serial /dev/ttyACM0")
         except Exception as e:
             self.get_logger().error(f"Serial Error: {e}")
             raise e
 
-        # 2. Subscribe to Joint Trajectory
+        # subscribe to joint_trajectory
         self.subscription = self.create_subscription(
             JointTrajectory,
             '/joint_trajectory',
@@ -24,20 +24,20 @@ class TrajectoryToSerialNode(Node):
             10)
 
     def listener_callback(self, msg):
-        # 3. Check if trajectory has points
+        # get the treajectory points
         if msg.points:
             # Get the target positions from the first point in the trajectory
             point = msg.points[0]
             
-            # 4. Format and Send (comma-separated string)
+            # format the commands
             # this needs to be changed to actually match the units the servos use
-            # Convert radians (ROS) to degrees
+            # convert radians to degrees
             pos_deg = [int(math.degrees(p)) for p in point.positions]
             vel_deg = [int(math.degrees(v)) for v in point.velocities]
             
-            # Format string for broadcast frame: $P1,P2,P3,P4,P5,P6,V1,V2,V3,V4,V5,V6\n
+            # format string for broadcast: $P1,P2,P3,P4,P5,P6,V1,V2,V3,V4,V5,V6\n, but what if there is accidentally more than 6 values for each?
             command = f"${','.join(map(str, pos_deg))},{','.join(map(str, vel_deg))}\n"
-            #self.ser.write(command.encode())
+                        self.get_logger().info(f"Sending joint command {command}")
             
             
             try:
