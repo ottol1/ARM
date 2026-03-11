d601aaa4fd658baed78982107811e253cae9adfa
ottol1 <162608916+ottol1@users.noreply.github.com>
2026-03-11 09:04:09 -0700

    Refactor CameraWindow class for better streaming
    Updated camera window class to improve functionality and fix issues.

--- UI/camera_window.py
+++ UI/camera_window.py
@@ -1,39 +1,47 @@
 import customtkinter
 import cv2
-
+from PIL import Image 
 #----------------------------
 #	create new window
-class camerawindow(customtkinter.CTkToplevel):
+class CameraWindow(customtkinter.CTkToplevel):
 	def __init__(self):
 		super().__init__()
 
 		self.title("Camera Preview")
-		self.geometry("640x480")
+		self.geometry("640x500")
 
 		self.cap = cv2.VideoCapture(0)
+		self._running = True 
+
+		self.status = customtkinter.CTKLabel(self, text="Starting camera ")		
+		self.status.pack(fill="both", expand=True)
 
 		self.label = customtkinter.CTkLabel(self, text="")
 		self.label.pack(fill="both", expand=True)
 
+		self.status.configure(text="Camera running")	
 		self.update_frame()
 
 #----------------------------
-#	
+#	live feeed
 	def streaming(self):
+		if not self.running:
+			return
+
 		ret, img = self.cap.read()
-	
 		if ret:
-			frame = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
-			img = Image.fromarray(frame)
-			imgtk = ImageTk.PhotoImage(image=img)
+			rgb_frame = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
+			img = Image.fromarray(rgb_frame)
+			imgctk = customtkinter.CTKImage(image=img, size=(620,420))
 
-			self.label.imgtk = imgtk
-			self.label.configure(image=imgtk)
+			self.label.configure(image=imgctk)
+			self.label.imgtk = imgctk
 
-		self.after(2, self.update_frame)
-		print("Camera started successfully")
+		self.after(33, self.streaming)
 #----------------------------
 #	close new window 
 	def close(self):
-		self.cap.release()
+		self.running = False
+		if self.cap.isOpened()
+			self.cap.release()
 		self.destroy()
