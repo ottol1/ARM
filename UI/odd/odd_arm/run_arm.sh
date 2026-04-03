a107908a28ba7cc8ea096e865ab2253879b6f073
ELAlvarado <132153888+ELAlvarado@users.noreply.github.com>
2026-04-03 08:30:05 -0700

    Updated run_arm.sh

--- UI/odd/odd_arm/run_arm.sh
+++ UI/odd/odd_arm/run_arm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-source ros_workspace/install/local_setup.bash
+source ros_workspace/install/setup.bash
 
 ros2 launch arm_robot_moveit_config demo.launch.py --noninteractive
+sudo chmod 666 /dev/ttyACM0 # Make sure this is the OpenCM
 ros2 run arm_package opencm_command3 --noninteractive # Does this need to be noninteractive?
